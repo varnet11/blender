3f1027567d3442b831ec165a07b5c3d15936b893
Hans Goudey <h.goudey@me.com>
2022-12-14 14:34:20 -0600

    Add initial simulation state items array to output node

--- source/blender/nodes/geometry/nodes/node_geo_simulation_output.cc
+++ source/blender/nodes/geometry/nodes/node_geo_simulation_output.cc
@@ -26,8 +26,27 @@ static void node_declare(NodeDeclarationBuilder &b)
 static void node_init(bNodeTree * /*tree*/, bNode *node)
 {
   NodeGeometrySimulationOutput *data = MEM_cnew<NodeGeometrySimulationOutput>(__func__);
+  data->state_items = MEM_cnew_array<SimulationStateItem>(1, __func__);
+  data->state_items[0].name = BLI_strdup(DATA_("Geometry"));
+  data->state_items[0].data_type = SOCK_GEOMETRY;
+  data->state_items_num = 1;
   data->use_persistent_cache = false;
   node->storage = data;
+}
+
+static void node_free_storage(bNode *node)
+{
+  NodeGeometrySimulationOutput &storage = node_storage(*node);
+  MEM_SAFE_FREE(storage.state_items);
+}
+
+void node_copy_storage(bNodeTree * /*dest_ntree*/, bNode *dst_node, const bNode *src_node)
+{
+  const NodeGeometrySimulationOutput &src = node_storage(*src_node);
+  NodeGeometrySimulationOutput &dst = node_storage(*dst_node);
+  MEM_SAFE_FREE(dst.state_items);
+  dst.state_items = MEM_cnew_array<SimulationStateItem>(src.state_items_num, __func__);
+  dst.state_items_num = src.state_items_num;
 }
 
 static void node_geo_exec(GeoNodeExecParams params)
@@ -90,9 +109,9 @@ void register_node_type_geo_simulation_output()
   ntype.declare = file_ns::node_declare;
   node_type_storage(&ntype,
                     "NodeGeometrySimulationOutput",
-                    node_free_standard_storage,
-                    node_copy_standard_storage);
+                    file_ns::node_free_storage,
+                    file_ns::node_copy_storage);
   ntype.geometry_node_execute_supports_laziness = true;
-  // ntype.declaration_is_dynamic = true;
+  ntype.declaration_is_dynamic = true;
   nodeRegisterType(&ntype);
 }
